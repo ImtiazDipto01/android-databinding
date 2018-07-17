670c8ffa6696eaa15748d769a1c7aac758a2dd41
imtiaz <programmer.imtiaz@gmail.com>
2018-07-17 08:57:07 +0600

    ADD- DataBinding onClicked action performed added

--- app/src/main/java/com/example/dipto/databindingpractice/normaldatabinding/FirstActivity.java
+++ app/src/main/java/com/example/dipto/databindingpractice/normaldatabinding/FirstActivity.java
@@ -1,8 +1,11 @@
 package com.example.dipto.databindingpractice.normaldatabinding;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.dipto.databindingpractice.R;
 import com.example.dipto.databindingpractice.databinding.ActivityFirstBinding;
@@ -10,6 +13,7 @@ import com.example.dipto.databindingpractice.databinding.ActivityFirstBinding;
 public class FirstActivity extends AppCompatActivity {
 
     private UserPojo user ;
+    private MyClickHandlers handlers ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,21 @@ public class FirstActivity extends AppCompatActivity {
         user.setEmail("Imtiaz@gmail.com");
         user.setName("Imtiaz Uddin Ahmed");
         binding.setUser(user);
+
+        handlers = new MyClickHandlers(this) ;
+        binding.setHandlers(handlers);
+    }
+
+    public class MyClickHandlers {
+
+        Context context;
+
+        public MyClickHandlers(Context context) {
+            this.context = context;
+        }
+
+        public void onEmailClicked(View view) {
+            Toast.makeText(getApplicationContext(), "Email Clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
