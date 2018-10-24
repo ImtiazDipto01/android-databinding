a7d3c11a79d26b03dd5848fdda0cde7188ec26e0
imtiaz <programmer.imtiaz@gmail.com>
2018-10-25 01:43:06 +0600

    ADD- user profile imae setting left

---
+++ app/src/main/java/com/example/dipto/databindingpractice/userprofile/UserProfile.java
@@ -0,0 +1,32 @@
+package com.example.dipto.databindingpractice.userprofile;
+
+public class UserProfile {
+
+    private String name ;
+    private int age ;
+    private String image ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/dipto/databindingpractice/userprofile/UserProfileActivity.java
+++ app/src/main/java/com/example/dipto/databindingpractice/userprofile/UserProfileActivity.java
@@ -1,15 +1,25 @@
 package com.example.dipto.databindingpractice.userprofile;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.dipto.databindingpractice.R;
+import com.example.dipto.databindingpractice.databinding.ActivityUserProfileBinding;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    UserProfile userProfile ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
+        ActivityUserProfileBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_user_profile);
+
+        userProfile = new UserProfile();
+        userProfile.setName("Imtiaz Uddin Ahmed");
+        userProfile.setAge(25);
+
+        binding.setUserprofile(userProfile);
     }
 }
