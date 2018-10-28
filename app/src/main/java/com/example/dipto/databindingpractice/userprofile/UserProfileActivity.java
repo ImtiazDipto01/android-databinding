1a87289443bd3716c602499cb5208f8c8631b82f
imtiaz <programmer.imtiaz@gmail.com>
2018-10-28 19:37:25 +0600

    ADD- image loading done with binding adapter

--- app/src/main/java/com/example/dipto/databindingpractice/userprofile/UserProfileActivity.java
+++ app/src/main/java/com/example/dipto/databindingpractice/userprofile/UserProfileActivity.java
@@ -1,9 +1,12 @@
 package com.example.dipto.databindingpractice.userprofile;
 
+import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.dipto.databindingpractice.R;
 import com.example.dipto.databindingpractice.databinding.ActivityUserProfileBinding;
 
@@ -20,6 +23,14 @@ public class UserProfileActivity extends AppCompatActivity {
         userProfile = new UserProfile();
         userProfile.setName("Imtiaz Uddin Ahmed");
         userProfile.setAge(25);
+        userProfile.setImage("https://picsum.photos/300/300/?image=267");
         binding.setUserprofile(userProfile);
+    }
+
+    @BindingAdapter("imageUrl")
+    public static void loadImage(ImageView iv, String url){
+        Glide.with(iv.getContext())
+                .load(url)
+                .into(iv);
     }
 }
