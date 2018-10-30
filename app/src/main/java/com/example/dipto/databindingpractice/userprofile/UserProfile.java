548654333c6223735c9f0276b88fa6c6036cecfe
imtiaz <programmer.imtiaz@gmail.com>
2018-10-31 00:48:19 +0600

    ADD- BaseObservable added into UserPojo

--- app/src/main/java/com/example/dipto/databindingpractice/userprofile/UserProfile.java
+++ app/src/main/java/com/example/dipto/databindingpractice/userprofile/UserProfile.java
@@ -1,17 +1,24 @@
 package com.example.dipto.databindingpractice.userprofile;
 
-public class UserProfile {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.example.dipto.databindingpractice.BR;
+
+public class UserProfile extends BaseObservable {
 
     private String name ;
     private int age ;
     private String image ;
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
     public int getAge() {
