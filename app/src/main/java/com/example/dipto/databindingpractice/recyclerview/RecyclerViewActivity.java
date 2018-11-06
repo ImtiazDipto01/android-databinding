eed69e4fcd39ac5386be0a8172c748b037054f81
imtiaz <programmer.imtiaz@gmail.com>
2018-11-07 01:49:50 +0600

    ADD- Recyclerview Activity Added

---
+++ app/src/main/java/com/example/dipto/databindingpractice/recyclerview/RecyclerViewActivity.java
@@ -0,0 +1,18 @@
+package com.example.dipto.databindingpractice.recyclerview;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dipto.databindingpractice.R;
+import com.example.dipto.databindingpractice.databinding.ActivityRecyclerViewBinding;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+    }
+}
